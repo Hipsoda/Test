39da7d0846a2a0ca8f3ca7f880f5823d7a0bc963
Hipsoda <143324009+Hipsoda@users.noreply.github.com>
2024-05-03 23:41:06 +0800

    rewrite the Helloworld.py

--- Helloworld.py
+++ Helloworld.py
@@ -13,4 +13,14 @@ list1 = range(1, 4)
 list2 = [i*i for i in list1]
 print(list2)
 
-#
+#3. decoration
+import time
+def time_cnt(func):
+    def wrapper(*argc, **kwargv):
+        start = time.time()
+        res = func(*argc, **kwargv)
+        end = time.time()
+        print("The function takes " + str(end - start) + " second")
+        return res
+    return wrapper
+
