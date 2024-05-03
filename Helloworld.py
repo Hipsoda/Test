b443432b5287e07757985d770c97469ff6fbb928
Hipsoda <143324009+Hipsoda@users.noreply.github.com>
2024-05-03 23:03:26 +0800

    second version of Helloworld.py

--- Helloworld.py
+++ Helloworld.py
@@ -1,1 +1,15 @@
-print("hello git")
+#Grammar Sugar in Python
+
+#1. swap two numbers
+
+x = 1
+y = 2
+x, y = y, x#this helps swap two numbers
+print(x, y)
+
+
+#2. List Comprehesion
+list1 = range(1, 4)
+list2 = [i*i for i in list1]
+print(list2)
+
